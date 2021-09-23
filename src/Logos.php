ea4e740e9c02965e179a99bc14c9061dbab9be38
develhopper <alireza.tjd77@gmail.com>
2021-09-23 10:18:30 +0330

    write documentation

--- src/Logos.php
+++ src/Logos.php
@@ -2,13 +2,41 @@
 
 namespace develhopper\Logos;
 
+/**
+ * The Logos Class
+ * 
+ * This class is a simple kernel used for comiunicate with a web based shell program 
+ * 
+ * @author Develhopper <alireza.tjd77@gmail.com>
+ */
 class Logos{
 
+    /**
+     * Singleton Instance
+     *
+     * @var Logos
+     */
     private static $instance = null;
 
+    /**
+     * Array of registered commands
+     *
+     * @var array
+     */
     private $commands = [];
 
+    /**
+     * This is a Clousre pointer and will execute before run method
+     * 
+     * @var callable
+     */
     private $beforeRun = null;
+    
+    /**
+     * This is a Clousre pointer and will execute after run method
+     * 
+     * @var callable
+     */
     private $afterRun = null;
 
     private function __construct(){}
@@ -21,7 +49,14 @@ class Logos{
         return self::$instance;
     }
 
-    public function register($regex, $closure){
+    /**
+     * Register new command
+     *
+     * @param string $regex
+     * @param callable $closure
+     * @return void
+     */
+    public function register(string $regex, callable $closure) : void {
         $regex = preg_replace('/\{.*?\}/', '(.+)', $regex);
         array_push($this->commands, [
             'regex' => "/^$regex\$/",
@@ -29,15 +64,37 @@ class Logos{
         ]);
     }
 
-    public function beforeRun($closure){
+    /**
+     * Register beforeRun callable
+     *
+     * @param callable $closure
+     * @return void
+     */
+    public function beforeRun(callable $closure) : void{
         $this->beforeRun = $closure;
     }
 
-    public function afterRun($closure){
+    /**
+     * Register afterRun callable
+     *
+     * @param callable $closure
+     * @return void
+     */
+    public function afterRun(callable $closure){
         $this->afterRun = $closure;
     }
 
-    public function run($command){
+    /**
+     * if command string matches any registered commands then will execute it closure
+     * otherwise it will return -1
+     * 
+     * this method will run beforeRun closure before running
+     * and will run afterRun closure and pass result of command to it for modification
+     *  
+     * @param string $command
+     * @return mixed
+     */
+    public function run(string $command) : mixed {
         foreach($this->commands as $item){
             preg_match($item['regex'], $command, $matches);
             if($matches){    
@@ -58,7 +115,14 @@ class Logos{
         return -1;
     }
 
-    public function call_closure($closure, $params = []){
+    /**
+     * Call closures and callables
+     *
+     * @param callable $closure
+     * @param array $params
+     * @return mixed
+     */
+    public function call_closure(callable $closure, array $params = []) : mixed{
         if(is_array($closure))
             return call_user_func_array($closure, $params);
         else
--- tests/test.php
+++ tests/test.php
@@ -7,7 +7,22 @@ use develhopper\Logos\Logos;
 $kernel = Logos::getInstance();
 
 $kernel->register('cd {dir}', function($input){
-    var_dump($input);
+    return ['pwd' => $input];
 });
 
-$kernel->run('cd test');
+$kernel->beforeRun(function($command){
+    // Run before runing command
+    var_dump($command);
+});
+
+$kernel->afterRun(function($result){
+    // Run after running command for modification of results
+
+    array_push($result, 'new element');
+
+    return $result;
+});
+
+$result = $kernel->run('cd test');
+
+var_dump($result);
